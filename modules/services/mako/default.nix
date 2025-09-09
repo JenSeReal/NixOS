46d202c48acf86cc507a10fdfbbf169cc242dae2
Jens Plüddemann <jens@plueddemann.de>
2025-09-09 10:39:30 +0200

    mako

---
+++ modules/services/mako/default.nix
@@ -0,0 +1,42 @@
+{
+  delib,
+  lib,
+  pkgs,
+  ...
+}:
+delib.module {
+  name = "services.mako";
+
+  options = delib.singleEnableOption false;
+
+  nixos.ifEnabled = {...}: {
+    environment.systemPackages = with pkgs; [libnotify mako];
+
+    systemd.user.services.mako = {
+      after = ["graphical-session.target"];
+      description = "Mako notification daemon";
+      partOf = ["graphical-session.target"];
+      wantedBy = ["graphical-session.target"];
+      serviceConfig = {
+        Type = "dbus";
+        BusName = "org.freedesktop.Notifications";
+
+        ExecCondition = ''
+          ${lib.getExe pkgs.bash} -c '[ -n "$WAYLAND_DISPLAY" ]'
+        '';
+
+        ExecStart = ''
+          ${lib.getExe' pkgs.mako "mako"}
+        '';
+
+        ExecReload = ''
+          ${lib.getExe' pkgs.mako "makoctl"} reload
+        '';
+
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
+}
