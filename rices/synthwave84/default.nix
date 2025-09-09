5a456d1b12839ee8e533bff5313c3e394bdcac97
Jens Plüddemann <jens@plueddemann.de>
2025-09-09 15:55:39 +0200

    stylix

--- rices/synthwave84/default.nix
+++ rices/synthwave84/default.nix
@@ -1,4 +1,9 @@
-{delib, ...}: let
+{
+  delib,
+  pkgs,
+  lib,
+  ...
+}: let
   common = {
     enable = true;
     image = ./P13_Background1.png;
@@ -8,7 +13,74 @@ in
   delib.rice {
     name = "synthwave84";
 
-    nixos = common // {};
-
     darwin = common // {};
+
+    home =
+      common
+      // {
+        stylix = {
+          polarity = "dark";
+
+          cursor = {
+            package = pkgs.layan-cursors;
+            name = "layan-white-cursors";
+            size = 24;
+          };
+
+          opacity = {
+            desktop = 1.0;
+            applications = 0.90;
+            terminal = 0.90;
+            popups = 1.0;
+          };
+
+          # cursor = with pkgs; {
+          #   package = phinger-cursors;
+          #   name = "phinger-cursors-light";
+          #   size = 24;
+          # };
+
+          fonts = with pkgs; {
+            sizes = {
+              desktop = 11;
+              applications = 12;
+              terminal = 13;
+              popups = 12;
+            };
+
+            serif = {
+              package = monaspace;
+              name =
+                if stdenv.hostPlatform.isDarwin
+                then "Monaspace Xenon"
+                else "Monaspace Xenon";
+            };
+            sansSerif = {
+              package = monaspace;
+              name =
+                if stdenv.hostPlatform.isDarwin
+                then "Monaspace Argon"
+                else "Monaspace Argon";
+            };
+            monospace = {
+              package = monaspace;
+              name =
+                if pkgs.stdenv.hostPlatform.isDarwin
+                then "Monaspace Krypton"
+                else "Monaspace Krypton";
+            };
+            emoji = {
+              package = noto-fonts-color-emoji;
+              name = "Noto Color Emoji";
+            };
+          };
+
+          targets = {
+            vscode.profileNames = ["stylix"];
+            firefox.profileNames = ["stylix"];
+          };
+        };
+      };
+
+    nixos = common // {};
   }
