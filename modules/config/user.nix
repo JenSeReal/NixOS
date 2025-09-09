8ae2ea6f8ebe1768b521b1d3480f3cb14c5204c9
Jens Plüddemann <jens@plueddemann.de>
2025-09-09 15:35:37 +0200

    user

--- modules/config/user.nix
+++ modules/config/user.nix
@@ -3,61 +3,120 @@
   lib,
   pkgs,
   ...
-}:
-delib.module {
-  name = "user";
+}: let
+  defaultIconFileName = "icon.png";
+  defaultIcon = pkgs.stdenvNoCC.mkDerivation {
+    name = "default-icon";
+    src = ./. + "/${defaultIconFileName}";
 
-  options = with delib;
-    moduleOptions {
-      uid = intOption 501;
-      extraGroups = attrsOfOption str {};
-      extraOptions = attrsOfOption str {};
+    dontUnpack = true;
+
+    installPhase = ''
+      cp $src $out
+    '';
+
+    passthru = {
+      fileName = defaultIconFileName;
     };
+  };
+in
+  delib.module {
+    name = "user";
 
-  nixos.always = {
-    myconfig,
-    cfg,
-    ...
-  }: let
-    inherit (myconfig.constants) username;
-  in {
-    users.users.${username} =
+    options = with delib;
+      moduleOptions {
+        uid = intOption 501;
+        name = strOption myconfig.constants.username;
+        fullName = strOption myconfig.constants.fullName;
+        email = strOption myconfig.constants.email;
+        initialPassword = strOption "password";
+        icon = allowNull (packageOption null) defaultIcon;
+        extraGroups = attrsOfOption str {};
+        extraOptions = attrsOfOption str {};
+      };
+
+    nixos.always = {cfg, ...}: let
+      propagatedIcon =
+        pkgs.runCommandNoCC "propagated-icon"
+        {
+          passthru = {
+            inherit (cfg.icon) fileName;
+          };
+        }
+        ''
+          local target="$out/share/icons/user/${cfg.name}"
+          mkdir -p "$target"
+
+          cp ${cfg.icon} "$target/${cfg.icon.fileName}"
+        '';
+    in
       {
-        isNormalUser = true;
-        shell = pkgs.nushell;
-        uid = 1000;
-        home = "/home/${username}";
-        extraGroups =
-          [
-            "wheel"
-            "systemd-journal"
-            "mpd"
-            "audio"
-            "video"
-            "input"
-            "plugdev"
-            "lp"
-            "tss"
-            "power"
-            "nix"
-          ]
-          ++ cfg.extraGroups;
-      }
-      // cfg.extraOptions;
-  };
+        myconfig,
+        cfg,
+        ...
+      }: let
+        inherit (myconfig.constants) username;
+      in {
+        environment.systemPackages = [propagatedIcon];
 
-  darwin.always = {
-    myconfig,
-    cfg,
-    ...
-  }: let
-    inherit (myconfig.constants) username;
-  in {
-    users.users.${username} = {
-      uid = lib.mkIf (cfg.uid != null) cfg.uid;
-      name = username;
-      shell = pkgs.zsh;
-      home = "/Users/${username}";
+        myconfig.home = {
+          file = {
+            ".face".source = cfg.icon;
+            ".face.icon".source = cfg.icon;
+            "Desktop/.keep".text = "";
+            "Documents/.keep".text = "";
+            "Downloads/.keep".text = "";
+            "Music/.keep".text = "";
+            "Pictures/.keep".text = "";
+            "Videos/.keep".text = "";
+            "Pictures/${cfg.icon.fileName or (builtins.baseNameOf cfg.icon)}".source = cfg.icon;
+          };
+
+          configFile = {
+            "sddm/faces/.${cfg.name}".source = cfg.icon;
+          };
+        };
+
+        users.users.${username} =
+          {
+            inherit (cfg) name initialPassword;
+            isNormalUser = true;
+            shell = pkgs.nushell;
+            uid = 1000;
+            group = "users";
+            home = "/home/${username}";
+            extraGroups =
+              [
+                "wheel"
+                "systemd-journal"
+                "mpd"
+                "audio"
+                "video"
+                "input"
+                "plugdev"
+                "lp"
+                "tss"
+                "power"
+                "nix"
+              ]
+              ++ cfg.extraGroups;
+          }
+          // cfg.extraOptions;
+      };
+
+    darwin.always = {
+      myconfig,
+      cfg,
+      ...
+    }: let
+      inherit (myconfig.constants) username;
+    in {
+      users.users.${username} = {
+        inherit (cfg) name;
+
+        uid = lib.mkIf (cfg.uid != null) cfg.uid;
+        shell = pkgs.zsh;
+        home = "/Users/${username}";
+      };
     };
-  };
-}
+  }
