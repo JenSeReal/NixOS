d33ed5b6d0845af4845bfee5454c2ffa46558bc8
JenSeReal <jens@plueddemann.de>
2026-03-31 21:37:28 +0200

    nix-ld

--- modules/programs/nix-ld.nix
+++ modules/programs/nix-ld.nix
@@ -1,10 +1,37 @@
-{delib, ...}:
+{
+  delib,
+  pkgs,
+  ...
+}:
 delib.module {
   name = "programs.nix-ld";
   options = delib.singleEnableOption false;
 
   nixos.ifEnabled = {...}: {
     programs.nix-ld.enable = true;
-    programs.nix-ld.libraries = [];
+    programs.nix-ld.libraries = with pkgs;
+    with pkgs; [
+      freetype
+      fontconfig
+      stdenv.cc.cc.lib
+      glib
+      gtk3
+      libGL
+      vulkan-loader
+      xorg.libX11
+      xorg.libXext
+      xorg.libXrandr
+      xorg.libXi
+      xorg.libXcursor
+      xorg.libXcomposite
+      xorg.libXinerama
+      xorg.libxcb
+      wayland
+      dbus
+      openssl
+      libpulseaudio
+      alsa-lib
+      udev
+    ];
   };
 }
