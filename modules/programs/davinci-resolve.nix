40b620bbe7fe3bc434b22a1837d0745e1198551d
Jens Plüddemann <jens@plueddemann.de>
2026-04-06 23:27:31 +0200

    updates

--- modules/programs/davinci-resolve.nix
+++ modules/programs/davinci-resolve.nix
@@ -9,9 +9,31 @@ delib.module {
     moduleOptions {
       enable = boolOption false;
       package = packageOption pkgs.davinci-resolve;
+      amdgpu = {
+        enable = boolOption true;
+        opencl = boolOption true;
+      };
     };
 
   nixos.ifEnabled = {cfg, ...}: {
-    environment.systemPackages = [cfg.package];
+    environment.systemPackages =
+      [cfg.package]
+      ++ (
+        if cfg.amdgpu.enable
+        then with pkgs; [
+          rocmPackages.clr
+          rocmPackages.rocminfo
+          rocmPackages.rocm-runtime
+        ]
+        else []
+      );
+
+    hardware.graphics.extraPackages =
+      if cfg.amdgpu.opencl
+      then with pkgs; [
+        rocmPackages.clr
+        rocmPackages.clr.icd
+      ]
+      else [];
   };
 }
